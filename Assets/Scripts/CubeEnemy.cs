851419ea6a88e04122928b830762c12f8e6e9f9f
Nshan Petrosyan <35226358+Nishkaa@users.noreply.github.com>
2021-08-04 12:51:39 +0300

    New Dark Cube Enemy

---
+++ Assets/Scripts/CubeEnemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+public class CubeEnemy : MonoBehaviour
+{
+    private Vector3 moveDirection = Vector3.zero;
+    public Transform Player;
+    public float begin;
+    public float dist = 5;
+    public float speed = 5;
+    public int dir;
+    // Start is called before the first frame update
+    void Start()
+    {
+        begin = transform.position.x;
+        dir = 1;
+    }
+
+    void Update()
+    {
+       
+                
+        if (transform.position.x > begin + dist) { dir = -1; }
+        else { if (transform.position.x < begin - dist) { dir = 1; } }
+        transform.position = new Vector3(transform.position.x + Time.deltaTime * speed * dir, transform.position.y, transform.position.z);
+    }
+    void OnCollisionEnter(Collision col)
+    {
+      if (col.gameObject.name == "Player")
+        {
+            Destroy(GameObject.FindWithTag("Player"));
+            SceneManager.LoadScene("TheWorld");
+        }
+    }
+
+}
---
+++ Assets/Scripts/PouseGame.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PouseGame : MonoBehaviour
+{
+    public bool gamePaused = false;
+    public GameObject pauseMenu;
+    public GameObject MainMenu;
+    public GameObject Restart;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if(gamePaused == false)
+            {
+                Time.timeScale = 0;
+                gamePaused = true;
+                Cursor.visible = true;
+                pauseMenu.SetActive(true);
+                MainMenu.SetActive(true);
+                Restart.SetActive(true);
+            }
+            else
+            {
+                pauseMenu.SetActive(false);
+                MainMenu.SetActive(false);
+                Restart.SetActive(false);
+                Cursor.visible = false;
+                gamePaused = false;
+                Time.timeScale = 1;
+            }
+        }
+    }
+}
